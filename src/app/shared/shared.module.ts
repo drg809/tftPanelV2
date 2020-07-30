f32ac737943e400c951cd2c5020fc5158d5690f9
Daniel Rodriguez <drg809@gmail.com>
2020-07-30 09:58:19 +0200

    shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+
+import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  declarations: [
+
+  ],
+  exports: [
+
+  ],
+  providers: [
+  ],
+  schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
+})
+export class SharedModule { }
