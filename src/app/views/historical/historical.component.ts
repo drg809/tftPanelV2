d86c2e344001926b6098f671a2571946e49696bb
Daniel Rodriguez <drg809@gmail.com>
2020-07-31 22:45:38 +0200

    moved to views

---
+++ src/app/views/historical/historical.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { SummonerService } from '../../shared/services/summoners.service';
+import { SumMatch } from '../../shared/models/match';
+import { User } from '../../shared/models/user';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-historical',
+  templateUrl: './historical.component.html',
+  styleUrls: ['./historical.component.scss']
+})
+export class HistoricalComponent implements OnInit {
+  matchs: SumMatch[];
+  user: User;
+
+  constructor(private summonerService: SummonerService,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('currentUser') || '{}');
+    this.summonerService.getMatchesHistoric(this.user._id).subscribe(data => {
+      this.matchs = data;
+      this.matchs.sort((a, b) => b.data?.info.game_datetime  - a.data?.info.game_datetime);
+      console.log(this.matchs);
+    });
+  }
+
+  navigateToMatch(entrie: string) {
+    console.log(entrie);
+    this.router.navigate(['/historical/' + entrie]);
+  }
+}
---
+++ src/app/views/profile/profile.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { SummonerService } from '../../shared/services/summoners.service';
+import { Summoner } from '../../shared/models/summoner';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class Profile1Component implements OnInit {
+  summoners: any;
+  newSummoner: Summoner;
+  summonerName: string;
+  user: any;
+  constructor(private summonerService: SummonerService) { }
+
+  ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('currentUser') || '{}');
+    this.summonerService.getByUserId(this.user._id).subscribe(data => {
+      this.summoners = data;
+    });
+  }
+
+  addNewSummoner() {
+    this.summonerName !== undefined ? this.summonerService.create({userId: this.user._id, summonerName: this.summonerName}).subscribe((emitData: any) => { console.log(emitData); }) : console.log('Introduce tu nombre de invocador');
+  }
+
+  getMatches(userId: any) {
+    this.summonerService.getMatches(userId).subscribe((emitData: any) => { console.log(emitData); });
+  }
+
+  setLastMatch(userId: any) {
+    this.summonerService.setLastMatchInfo(userId).subscribe((emitData: any) => { console.log(emitData); });
+  }
+
+  deleteSummoner(id: string) {
+    this.summonerService.remove(id).subscribe((emitData: any) => { console.log(emitData); });
+  }
+}
