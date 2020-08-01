34a744807915a95ee36dabc17790021794bd0e7e
Daniel Rodriguez <drg809@gmail.com>
2020-08-01 06:15:31 +0200

    match component first tab

--- src/app/views/historical/match/historical-match.component.ts
+++ src/app/views/historical/match/historical-match.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SummonerService } from '../../../shared/services/summoners.service';
+import { SumMatch } from 'app/shared/models/match';
+import { User } from 'app/shared/models/user';
 
 @Component({
   selector: 'app-historical-match',
@@ -9,23 +11,18 @@ import { SummonerService } from '../../../shared/services/summoners.service';
 })
 export class HistoricalMatchComponent implements OnInit {
   entrieId: string | null;
-  tabLoadTimes: Date[] = [];
+  entrie: SumMatch;
+  user: User;
 
   constructor(private route: ActivatedRoute,
               private summonerService: SummonerService) { }
 
   ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('currentUser') || '{}');
     this.entrieId = this.route.snapshot.paramMap.get('id');
     this.summonerService.getMatchInfo(this.entrieId).subscribe(data => {
       console.log(data);
+      this.entrie = data;
     });
-  }
-
-  getTimeLoaded(index: number) {
-    if (!this.tabLoadTimes[index]) {
-      this.tabLoadTimes[index] = new Date();
-    }
-
-    return this.tabLoadTimes[index];
   }
 }
