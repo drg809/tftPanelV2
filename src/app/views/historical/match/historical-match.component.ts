2dd4ae70f0c4213f0c5baaa951cc428b3aea29d1
Daniel Rodriguez <drg809@gmail.com>
2020-08-01 20:15:10 +0200

    desing match view

--- src/app/views/historical/match/historical-match.component.ts
+++ src/app/views/historical/match/historical-match.component.ts
@@ -23,6 +23,24 @@ export class HistoricalMatchComponent implements OnInit {
     this.summonerService.getMatchInfo(this.entrieId).subscribe(data => {
       console.log(data);
       this.entrie = data;
+      this.entrie.data.info.participants.sort((a, b) => a.placement  - b.placement);
+      this.entrie.data.info.participants.forEach(element => {
+        element.units.sort((a, b) => a.rarity - b.rarity);
+      });
+      this.entrie.data.info.participants.forEach(element => {
+        element.units.sort((a, b) => {
+          const aRarity = a.rarity;
+          const bRarity = b.rarity;
+          const aName = a.character_id;
+          const bName = b.character_id;
+          console.log(aRarity + ' | ' + bRarity);
+          if (aRarity == bRarity) {
+            return (aName < bName) ? -1 : (aName > bName) ? 1 : 0;
+          } else {
+            return (aRarity < bRarity) ? -1 : 1;
+          }
+        });
+      });
     });
   }
 }
