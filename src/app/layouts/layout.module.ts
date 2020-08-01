cced03d64045fffe84b9151c83a9f8afc50fefdc
Daniel Rodriguez <drg809@gmail.com>
2020-08-01 05:39:00 +0200

    new routes and components loaded

--- src/app/layouts/layout.module.ts
+++ src/app/layouts/layout.module.ts
@@ -5,9 +5,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LayoutRoutes } from './layout.routing';
 import { DashboardComponent } from '../views/dashboard/dashboard.component';
 import { UserProfileComponent } from '../views/user-profile/user-profile.component';
-import { NotificationsComponent } from '../notifications/notifications.component';
+import { NotificationsComponent } from '../shared/notifications/notifications.component';
 import { HistoricalComponent } from 'app/views/historical/historical.component';
 import { AngularMaterialModule } from 'app/angular-material.modules';
+import { ProfileComponent } from 'app/views/profile/profile.component';
 
 @NgModule({
   imports: [
@@ -21,7 +22,6 @@ import { AngularMaterialModule } from 'app/angular-material.modules';
     DashboardComponent,
     UserProfileComponent,
     NotificationsComponent,
-    HistoricalComponent
   ],
   exports: [
     AngularMaterialModule
--- src/app/layouts/layout.routing.ts
+++ src/app/layouts/layout.routing.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 
 import { DashboardComponent } from '../views/dashboard/dashboard.component';
 import { UserProfileComponent } from '../views/user-profile/user-profile.component';
-import { NotificationsComponent } from '../notifications/notifications.component';
+import { NotificationsComponent } from '../shared/notifications/notifications.component';
 import { HistoricalComponent } from 'app/views/historical/historical.component';
 import { HistoricalMatchComponent } from 'app/views/historical/match/historical-match.component';
+import { ProfileComponent } from 'app/views/profile/profile.component';
 
 export const LayoutRoutes: Routes = [
     { path: 'dashboard',      component: DashboardComponent },
     { path: 'user-profile',   component: UserProfileComponent },
+    { path: 'profile',   component: ProfileComponent },
     { path: 'notifications',  component: NotificationsComponent },
     { path: 'historical',  component: HistoricalComponent },
     { path: 'historical/:id',  component: HistoricalMatchComponent },
