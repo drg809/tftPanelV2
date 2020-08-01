0bca570fd91db6a53df81cec7be1366528f66cbe
Daniel Rodriguez <drg809@gmail.com>
2020-08-01 21:33:08 +0200

    model match updated

--- src/app/shared/models/match.ts
+++ src/app/shared/models/match.ts
@@ -16,6 +16,7 @@ export class SumMatch {
       puuid: string,
       time_eliminated: number,
       total_damage_to_players: number,
+      r: string,
       traits: [{
         name: string,
         num_units: number,
