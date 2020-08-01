7a118372a8daa8544ec73c2cd034fb165f242645
Daniel Rodriguez <drg809@gmail.com>
2020-08-01 16:15:11 +0200

    match model updated

--- src/app/shared/models/match.ts
+++ src/app/shared/models/match.ts
@@ -7,7 +7,30 @@ export class SumMatch {
     game_length: number,
     game_variation: string,
     game_version: string,
-    participants: object,
+    participants: [{
+      gold_left: number,
+      last_round: number,
+      level: number,
+      placement: number,
+      players_eliminated: number,
+      puuid: string,
+      time_eliminated: number,
+      total_damage_to_players: number,
+      traits: [{
+        name: string,
+        num_units: number,
+        style: number,
+        tier_current: number,
+        tier_total: number
+      }],
+      units: [{
+        character_id: string,
+        items: number[],
+        name: string,
+        rarity: number,
+        tier: number
+      }]
+    }],
     queue_id: number,
     tft_set_number: number
   } };
