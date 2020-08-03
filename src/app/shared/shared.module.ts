60c1f597f69f0b66523e163bcac79fa15afe542e
Daniel Rodriguez <drg809@gmail.com>
2020-08-03 04:46:32 +0200

    load dialog component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,18 @@
 
 import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ConfirmDialogsComponent } from './dialogs/confirm-dialog/confirm-dialog.component';
+import { AngularMaterialModule } from 'app/angular-material.modules';
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
+    AngularMaterialModule
   ],
   declarations: [
-
+    ConfirmDialogsComponent
   ],
   exports: [
 
