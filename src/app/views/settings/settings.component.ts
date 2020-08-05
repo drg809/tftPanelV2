457f02ada392a5af1bd0eb0faf2e3c25024be9ae
Daniel Rodriguez <drg809@gmail.com>
2020-08-05 06:02:54 +0200

    functions to call leaguesEntries

--- src/app/views/settings/settings.component.ts
+++ src/app/views/settings/settings.component.ts
@@ -1,3 +1,5 @@
+import { SummonersStats } from './../../shared/models/summonersStats';
+import { SummonerService } from './../../shared/services/summoners.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from 'app/shared/models/user';
 
@@ -8,11 +10,27 @@ import { User } from 'app/shared/models/user';
 })
 export class SettingsComponent implements OnInit {
   user: User;
+  leagueEntries: SummonersStats[];
+  apexLeagueEntries: SummonersStats[];
 
-  constructor() { }
+  constructor(private summonerService: SummonerService) { }
 
   ngOnInit() {
     this.user = JSON.parse(localStorage.getItem('currentUser') || '{}');
+  }
+
+  getLeagueEntries() {
+    this.summonerService.getLeaguesEntries().subscribe(data => {
+      this.leagueEntries = data.sort((a, b) => b.leaguePoints  - a.leaguePoints);
+      console.log(this.leagueEntries);
+    });
+  }
+
+  getApexLeagueEntries() {
+    this.summonerService.getApexLeaguesEntries().subscribe(data => {
+      this.apexLeagueEntries = data.sort((a, b) => b.leaguePoints  - a.leaguePoints);
+      console.log(this.apexLeagueEntries);
+    });
   }
 
 }
