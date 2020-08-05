84403c37e2d84e01bb46d602c089220faae18353
Daniel Rodriguez <drg809@gmail.com>
2020-08-05 05:58:47 +0200

    summoners stats model

---
+++ src/app/shared/models/summonersStats.ts
@@ -0,0 +1,17 @@
+export class SummonersStats {
+  public id?: string;
+  public leagueId?: string;
+  public summonerId?: string;
+  public summonerName?: string;
+  public queueType?: string;
+  public tier?: string;
+  public rank?: string;
+  public leaguePoints?: number;
+  public wins?: number;
+  public losses?: number;
+  public hotStreak?: boolean;
+  public veteran?: boolean;
+  public freshBlood?: boolean;
+  public inactive?: boolean;
+  public miniSeries?: object
+}
