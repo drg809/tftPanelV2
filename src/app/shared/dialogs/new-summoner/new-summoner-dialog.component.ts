70f780eed0954e263fd9b37913ff934200695992
Daniel Rodriguez <drg809@gmail.com>
2020-08-06 04:15:32 +0200

    dialog with variables

--- src/app/shared/dialogs/new-summoner/new-summoner-dialog.component.ts
+++ src/app/shared/dialogs/new-summoner/new-summoner-dialog.component.ts
@@ -1,6 +1,5 @@
 import {Component, Inject, NgZone, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-summoner-dialog',
@@ -16,14 +15,12 @@ export class NewSummonerDialogComponent implements OnInit {
   acceptButtonLabel = '';
   loading = false;
   action: 'danger';
-  formGroup: FormGroup;
+  name: string;
 
 
   constructor(public dialogRef: MatDialogRef<NewSummonerDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any,
-              private formBuilder: FormBuilder,
               private ngZone: NgZone) {
-    console.log(data);
     this.object = data.object;
     this.buttons = data.buttons;
     this.texts = data.texts;
@@ -32,27 +29,20 @@ export class NewSummonerDialogComponent implements OnInit {
 
   ngOnInit() {
     this.acceptButtonLabel = this.buttons.acceptButtonLabel;
-    this.createForm();
   }
 
-  createForm() {
-    let emailregex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    this.formGroup = this.formBuilder.group({
-      email: [null, [Validators.required, Validators.pattern(emailregex)]],
-      password: [null, Validators.required],
-      // 'password': [null, [Validators.required, this.checkPassword]],
-    });
-  }
+
 
   accept() {
     this.isLoading = true;
     this.acceptButtonLabel = this.buttons.acceptButtonLabelAccept;
-    setTimeout(() => {
-      this.ngZone.run(() => {
-        this.dialogRef.close(true);
-        this.loading = false;
-      });
-    }, 1000);
+    console.log(this.name);
+    // setTimeout(() => {
+    //   this.ngZone.run(() => {
+    //     this.dialogRef.close({res: true, name: this.summonerName});
+    //     this.loading = false;
+    //   });
+    // }, 1000);
   }
 
   cancel(): void {
