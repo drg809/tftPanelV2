56b846f38cec50dfba3a5e86671858a75fca5540
Daniel Rodriguez <drg809@gmail.com>
2020-08-08 06:26:30 +0200

    pagination added to historical view

--- src/app/views/historical/historical.component.ts
+++ src/app/views/historical/historical.component.ts
@@ -3,6 +3,7 @@ import { SummonerService } from '../../shared/services/summoners.service';
 import { SumMatch } from '../../shared/models/match';
 import { User } from '../../shared/models/user';
 import { Router } from '@angular/router';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-historical',
@@ -12,17 +13,31 @@ import { Router } from '@angular/router';
 export class HistoricalComponent implements OnInit {
   matchs: SumMatch[];
   user: User;
+  pageEvent: PageEvent;
+  pageIndex: number;
+  pageSize: number;
+  length: number;
 
   constructor(private summonerService: SummonerService,
               private router: Router) { }
 
   ngOnInit() {
     this.user = JSON.parse(localStorage.getItem('currentUser') || '{}');
-    this.summonerService.getMatchesHistoric(this.user._id).subscribe(data => {
-      this.matchs = data;
+    this.getServerData(null);
+  }
+
+  public getServerData(event?: PageEvent) {
+    console.log(event);
+    const pageI = event ? event.pageIndex : 0;
+    const params = {userId: this.user._id, page: pageI + 1}
+    this.summonerService.getMatchesHistoric(params).subscribe(data => {
+      this.matchs = data.data;
       this.matchs.sort((a, b) => b.data?.info.game_datetime  - a.data?.info.game_datetime);
-      console.log(this.matchs);
+      this.length = data.numResult;
+      this.pageIndex = data.pageIndex - 1;
+      this.pageSize = data.pageSize;
     });
+    return event;
   }
 
   navigateToMatch(entrie: string) {
