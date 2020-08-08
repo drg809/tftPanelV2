8a8e4049cfbfbbd602f0a178f9952da7c32af20b
Daniel Rodriguez <drg809@gmail.com>
2020-08-08 10:29:23 +0200

    customization of graphics and data

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { SummonersStats } from './../../shared/models/summonersStats';
+import { SummonersStats, SummonersStatsDetails } from './../../shared/models/summonersStats';
 import { SummonerService } from './../../shared/services/summoners.service';
 import { Component, OnInit } from '@angular/core';
 import * as Chartist from 'chartist';
@@ -6,14 +6,11 @@ import * as Chartist from 'chartist';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
   summonerStats: SummonersStats;
-  matchWon: number;
-  matchLosses: number;
-  matchTotal: number;
-  top4: number;
+  positions: SummonersStatsDetails;
 
   constructor(private summonerService: SummonerService) { }
   startAnimationForLineChart(chart) {
@@ -52,9 +49,9 @@ export class DashboardComponent implements OnInit {
   startAnimationForBarChart(chart) {
       let seq2: any, delays2: any, durations2: any;
 
-      seq2 = 0;
-      delays2 = 80;
-      durations2 = 500;
+      seq2 = 1;
+      delays2 = 200;
+      durations2 = 800;
       chart.on('draw', function(data) {
         if (data.type === 'bar') {
             seq2++;
@@ -70,46 +67,77 @@ export class DashboardComponent implements OnInit {
         }
       });
 
-      seq2 = 0;
+      seq2 = 1;
   };
 
   ngOnInit() {
       this.summonerService.getLeaguesEntries().subscribe((data: any) => {
         this.summonerStats = data;
-        this.matchWon = data.wins;
-        this.matchLosses = data.losses;
-        this.matchTotal = data.wins + data.losses;
-        console.log(data);
+        this.positions = data.positions;
+
+
+
+        setTimeout(() => {
+          const datawebsiteViewsChart = {
+            labels: ['Top1', 'Top2', 'Top3', 'Top4', 'Top5', 'Top6', 'Top7', 'Top8'],
+            series: [
+              [this.positions.top1, this.positions.top2, this.positions.top3, this.positions.top4, this.positions.top5, this.positions.top6, this.positions.top7, this.positions.top8]
+            ]
+          };
+          const optionswebsiteViewsChart = {
+              axisX: {
+                  showGrid: false
+              },
+              low: 0,
+              high: 30,
+              chartPadding: { top: 0, right: 0, bottom: 0, left: 0}
+          };
+          const responsiveOptions: any[] = [
+            ['screen and (max-width: 640px)', {
+              seriesBarDistance: 5,
+              axisX: {
+                labelInterpolationFnc: function (value) {
+                  return value[0];
+                }
+              }
+            }]
+          ];
+          const websiteViewsChart = new Chartist.Bar('#websiteViewsChart',
+                                                        datawebsiteViewsChart, optionswebsiteViewsChart, responsiveOptions);
+          this.startAnimationForBarChart(websiteViewsChart);
+        }, 100);
+
+        const dataDailySalesChart: any = {
+          labels: ['Top 1', 'Top 2', 'Top 3', 'Top 4', 'Top 5', 'Top 6', 'Top 7', 'Top 8'],
+          series: [
+              [this.positions.top1, this.positions.top2, this.positions.top3, this.positions.top4, this.positions.top5, this.positions.top6, this.positions.top7, this.positions.top8]
+          ]
+        };
+
+        const dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
+
+        this.startAnimationForLineChart(dailySalesChart);
+
       });
       /* ----------==========     Daily Sales Chart initialization For Documentation    ==========---------- */
 
-      const dataDailySalesChart: any = {
-          labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-          series: [
-              [12, 17, 7, 17, 23, 18, 38]
-          ]
-      };
+
 
      const optionsDailySalesChart: any = {
           lineSmooth: Chartist.Interpolation.cardinal({
               tension: 0
           }),
           low: 0,
-          high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          high: 30, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
           chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
       }
-
-      const dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
-
-      this.startAnimationForLineChart(dailySalesChart);
-
 
       /* ----------==========     Completed Tasks Chart initialization    ==========---------- */
 
       const dataCompletedTasksChart: any = {
           labels: ['12p', '3p', '6p', '9p', '12p', '3a', '6a', '9a'],
           series: [
-              [230, 750, 450, 300, 280, 240, 200, 190]
+              [20, 22, 25, 30, 24, 31, 18, 24]
           ]
       };
 
@@ -118,7 +146,7 @@ export class DashboardComponent implements OnInit {
               tension: 0
           }),
           low: 0,
-          high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          high: 40, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
           chartPadding: { top: 0, right: 0, bottom: 0, left: 0}
       }
 
@@ -131,35 +159,7 @@ export class DashboardComponent implements OnInit {
 
       /* ----------==========     Emails Subscription Chart initialization    ==========---------- */
 
-      const datawebsiteViewsChart = {
-        labels: ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'],
-        series: [
-          [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
 
-        ]
-      };
-      const optionswebsiteViewsChart = {
-          axisX: {
-              showGrid: false
-          },
-          low: 0,
-          high: 1000,
-          chartPadding: { top: 0, right: 5, bottom: 0, left: 0}
-      };
-      const responsiveOptions: any[] = [
-        ['screen and (max-width: 640px)', {
-          seriesBarDistance: 5,
-          axisX: {
-            labelInterpolationFnc: function (value) {
-              return value[0];
-            }
-          }
-        }]
-      ];
-      const websiteViewsChart = new Chartist.Bar('#websiteViewsChart', datawebsiteViewsChart, optionswebsiteViewsChart, responsiveOptions);
-
-      // start animation for the Emails Subscription Chart
-      this.startAnimationForBarChart(websiteViewsChart);
 
 
   }
