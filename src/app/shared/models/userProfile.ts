f6ffd98754e56c5fd2c0c775f85b7180b6d68a53
Daniel Rodriguez <drg809@gmail.com>
2020-08-09 20:24:44 +0200

    userId field added to model

--- src/app/shared/models/userProfile.ts
+++ src/app/shared/models/userProfile.ts
@@ -1,5 +1,6 @@
 export class UserProfile {
   public _id?: string;
+  public userId?:  string | null;
   public name?:  string | null;
   public lastname?: string | null;
   public country?: string | null;
