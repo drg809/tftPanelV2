4f0834a63b107e2f9f1da29b89e224a45d12fa6f
Daniel Rodriguez <drg809@gmail.com>
2020-08-09 08:49:11 +0200

    ts component working

--- src/app/views/user-profile/user-profile.component.ts
+++ src/app/views/user-profile/user-profile.component.ts
@@ -1,3 +1,5 @@
+import { UserProfileService } from './../../shared/services/usersProfile.service';
+import { UserProfile } from './../../shared/models/userProfile';
 import { Component, OnInit } from '@angular/core';
 import { User } from 'app/shared/models/user';
 
@@ -7,17 +9,20 @@ import { User } from 'app/shared/models/user';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-  user: User;
+  user: User; userProfile: UserProfile;
   name: string; lastname: string; phone: string; country: string; about: string;
 
-  constructor() { }
+  constructor(private userProfileService: UserProfileService) { }
 
   ngOnInit() {
     this.user = JSON.parse(localStorage.getItem('currentUser') || '{}');
   }
 
   onSaveProfile() {
-    console.log(1);
-  }
+    this.userProfile = {name: this.name, lastname: this.lastname, phone: this.phone, country: this.country, about: this.about};
+    this.userProfileService.create(this.userProfile).subscribe((x) => {
+      console.log(x);
+    });
+  };
 
 }
