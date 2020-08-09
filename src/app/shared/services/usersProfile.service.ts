deaf55a39e9c71d75152417d44a201499b1d6a74
Daniel Rodriguez <drg809@gmail.com>
2020-08-09 08:30:38 +0200

    user profile service

---
+++ src/app/shared/services/usersProfile.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {UserProfile} from '../models/userProfile';
+
+
+@Injectable({providedIn: 'root'})
+export class UserProfileService {
+  private apiUrl: string = environment.API_URL;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAll() {
+    return this.http.get<UserProfile[]>(this.apiUrl + '/profile');
+  }
+
+  getById(id: string) {
+    return this.http.get<UserProfile>(this.apiUrl + '/profile/' + id);
+  }
+
+  create(data: UserProfile) {
+    return this.http.post<UserProfile>(this.apiUrl + '/profile/', data);
+  }
+
+  update(data: UserProfile) {
+    return this.http.put<UserProfile>(this.apiUrl + '/profile/', data);
+  }
+
+  remove(id: string) {
+    return this.http.delete<any>(this.apiUrl + '/profile/' + id);
+  }
+}
