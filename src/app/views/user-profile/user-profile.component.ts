245d2bfe4e94486b1f67b3ab158b71169fb896df
Daniel Rodriguez <drg809@gmail.com>
2020-08-09 08:55:48 +0200

    use confirm dialog to save

--- src/app/views/user-profile/user-profile.component.ts
+++ src/app/views/user-profile/user-profile.component.ts
@@ -2,6 +2,10 @@ import { UserProfileService } from './../../shared/services/usersProfile.service
 import { UserProfile } from './../../shared/models/userProfile';
 import { Component, OnInit } from '@angular/core';
 import { User } from 'app/shared/models/user';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogsComponent } from 'app/shared/dialogs/confirm-dialog/confirm-dialog.component';
+import { sum } from 'chartist';
+import { Utils } from 'app/shared/helpers/utils';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,18 +14,37 @@ import { User } from 'app/shared/models/user';
 })
 export class UserProfileComponent implements OnInit {
   user: User; userProfile: UserProfile;
-  name: string; lastname: string; phone: string; country: string; about: string;
+  name: string; lastname: string; phone: string; country: string; about: string; obj: any;
 
-  constructor(private userProfileService: UserProfileService) { }
+  constructor(private userProfileService: UserProfileService, public dialog: MatDialog) { }
 
   ngOnInit() {
     this.user = JSON.parse(localStorage.getItem('currentUser') || '{}');
   }
 
   onSaveProfile() {
-    this.userProfile = {name: this.name, lastname: this.lastname, phone: this.phone, country: this.country, about: this.about};
-    this.userProfileService.create(this.userProfile).subscribe((x) => {
-      console.log(x);
+    this.obj = {
+      buttons: {
+          acceptButtonLabel : 'Guardar',
+          acceptButtonLabelAccept : 'Guardando'
+      },
+      texts: {
+          title: 'Guardar perfil',
+          text1: '¿Está seguro que desea ',
+          textBold: 'guardar ',
+          text2: 'el perfil'
+      },
+      action: 'danger'
+    };
+    this.dialog.open(ConfirmDialogsComponent, {
+      data: this.obj,
+    }).afterClosed().subscribe((result) => {
+      if (result) {
+        this.userProfile = {name: this.name, lastname: this.lastname, phone: this.phone, country: this.country, about: this.about};
+        this.userProfileService.create(this.userProfile).subscribe((x) => {
+          console.log(x);
+        });
+      }
     });
   };
 
