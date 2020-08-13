78628538053b514e83dc525709d810cff5b3576d
Daniel Rodriguez <drg809@gmail.com>
2020-08-13 21:49:47 +0200

    match notes service created

---
+++ src/app/shared/services/matchNotes.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {MatchNotes} from '../models/matchNotes';
+
+
+@Injectable({providedIn: 'root'})
+export class UserService {
+  private apiUrl: string = environment.API_URL;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getById(entrieId: string) {
+    return this.http.get<MatchNotes[]>(this.apiUrl + '/match_notes/' + entrieId);
+  }
+
+  create(data: MatchNotes) {
+    return this.http.post<MatchNotes>(this.apiUrl + '/match_notes/', data);
+  }
+
+  update(data: MatchNotes) {
+    return this.http.put<MatchNotes>(this.apiUrl + '/match_notes/', data);
+  }
+
+  remove(id: string) {
+    return this.http.delete<MatchNotes>(this.apiUrl + '/match_notes/' + id);
+  }
+}
