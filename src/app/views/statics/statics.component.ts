ee68b6b10b4924974ed94272be1006d52e95f5d9
Daniel Rodriguez <drg809@gmail.com>
2020-08-14 10:57:12 +0200

    statics scss and ts:

--- src/app/views/statics/statics.component.ts
+++ src/app/views/statics/statics.component.ts
@@ -10,12 +10,18 @@ import { ActivatedRoute } from '@angular/router';
 export class StaticsComponent implements OnInit {
   user: User;
   option: any;
+  rrss: any[];
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.user = JSON.parse(localStorage.getItem('currentUser') || '{}');
     this.option = this.route.snapshot.paramMap.get('option');
+    this.rrss = [
+      { name: 'Twitch', url: 'https://www.twitch.tv/mrmugui', res: '../../../assets/icons/twitch-brands.svg' },
+      { name: 'Twitter', url: 'https://twitter.com/mrmugui', res: '../../../assets/icons/twitter-square-brands.svg' },
+      { name: 'YouTube', url: 'https://www.youtube.com/channel/UCRklIYzE97Vds_cMUbSvDdg', res: '../../../assets/icons/youtube-square-brands.svg' }
+    ];
   }
 
 }
