a791f87b564d0e96d94949160120cab898e7bf87
Daniel Rodriguez <drg809@gmail.com>
2020-08-16 07:56:42 +0200

    save image base64

--- src/app/views/user-profile/user-profile.component.ts
+++ src/app/views/user-profile/user-profile.component.ts
@@ -1,3 +1,4 @@
+import { ImageDefault } from './../../shared/helpers/imageDefault';
 import { UserProfileService } from './../../shared/services/usersProfile.service';
 import { UserProfile } from './../../shared/models/userProfile';
 import { Component, OnInit } from '@angular/core';
@@ -14,6 +15,7 @@ import { Utils } from 'app/shared/helpers/utils';
 export class UserProfileComponent implements OnInit {
   user: User; userProfile: UserProfile;
   name: string; lastname: string; phone: string; country: string; about: string; obj: any;
+  imageSrc = ImageDefault.getImage();
 
   constructor(private userProfileService: UserProfileService, public dialog: MatDialog) { }
 
@@ -26,6 +28,48 @@ export class UserProfileComponent implements OnInit {
       this.country = x.country;
       this.about = x.about;
     });
+  }
+
+  handleInputChange(e) {
+    const file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
+    const pattern = /image-*/;
+    const reader = new FileReader();
+    if (!file.type.match(pattern)) {
+      Utils.showNotification('top', 'right', 'danger', 'Solo puedes subir una imagen.');
+      return;
+    }
+    reader.onload = this._handleReaderLoaded.bind(this);
+    reader.readAsDataURL(file);
+  }
+  _handleReaderLoaded(e) {
+    const reader = e.target;
+    this.imageSrc = reader.result;
+    // this.obj = {
+    //   buttons: {
+    //       acceptButtonLabel : 'Guardar',
+    //       acceptButtonLabelAccept : 'Guardando'
+    //   },
+    //   texts: {
+    //       title: 'Guardar imagen',
+    //       text1: '¿Está seguro que desea ',
+    //       textBold: 'guardar ',
+    //       text2: 'la nueva imagen de perfil'
+    //   },
+    //   action: 'danger'
+    // };
+    // this.dialog.open(ConfirmDialogsComponent, {
+    //   data: this.obj,
+    // }).afterClosed().subscribe((result) => {
+    //   if (result) {
+    //     this.userProfile = {userId: this.user._id, name: this.name, lastname: this.lastname, phone: this.phone, country: this.country, about: this.about};
+    //     this.userProfileService.create(this.userProfile).subscribe((x) => {
+    //       console.log(x);
+    //       Utils.showNotification('top', 'right', 'success', 'Perfil guardado correctamente.');
+    //     });
+    //   } else {
+    //     this.imageSrc = ImageDefault.getImage();
+    //   }
+    // });
   }
 
   onSaveProfile() {
