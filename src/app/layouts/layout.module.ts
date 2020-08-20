dddd5f9c4d7cf56493968b6d302e570ad359a3c4
Daniel Rodriguez <drg809@gmail.com>
2020-08-20 05:51:39 +0200

    components in views load in views module

--- src/app/layouts/layout.module.ts
+++ src/app/layouts/layout.module.ts
@@ -1,20 +1,15 @@
-import { TransformNamePipe, ItemCoreChampsPipe } from 'app/shared/helpers/utils';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LayoutRoutes } from './layout.routing';
-import { DashboardComponent } from '../views/dashboard/dashboard.component';
 import { UserProfileComponent } from '../views/user-profile/user-profile.component';
 import { NotificationsComponent } from '../shared/notifications/notifications.component';
-import { HistoricalComponent } from 'app/views/historical/historical.component';
 import { AngularMaterialModule } from 'app/angular-material.modules';
 import { ProfileComponent } from 'app/views/profile/profile.component';
-import { HistoricalMatchComponent } from 'app/views/historical/match/historical-match.component';
 import { SettingsComponent } from 'app/views/settings/settings.component';
 import { DatabaseComponent } from 'app/views/database/databse.component';
 import { StaticsComponent } from 'app/views/statics/statics.component';
-import { SearchSumComponent } from 'app/views/search-sum/search-sum.component';
 
 @NgModule({
   imports: [
@@ -25,23 +20,15 @@ import { SearchSumComponent } from 'app/views/search-sum/search-sum.component';
     AngularMaterialModule
   ],
   declarations: [
-    DashboardComponent,
     UserProfileComponent,
     NotificationsComponent,
-    HistoricalComponent,
-    HistoricalMatchComponent,
     ProfileComponent,
     SettingsComponent,
     DatabaseComponent,
-    TransformNamePipe,
-    StaticsComponent,
-    ItemCoreChampsPipe,
-    SearchSumComponent
+    StaticsComponent
   ],
   exports: [
-    AngularMaterialModule,
-    TransformNamePipe,
-    ItemCoreChampsPipe
+    AngularMaterialModule
   ]
 })
 
