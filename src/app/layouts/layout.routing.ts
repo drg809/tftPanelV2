80b075a2694bcc4e85b44dfaf08950eb3cb8f5eb
Daniel Rodriguez <drg809@gmail.com>
2020-08-20 06:20:06 +0200

    routed load

--- src/app/layouts/layout.routing.ts
+++ src/app/layouts/layout.routing.ts
@@ -10,6 +10,7 @@ import { ProfileComponent } from 'app/views/profile/profile.component';
 import { SettingsComponent } from 'app/views/settings/settings.component';
 import { SearchSumComponent } from 'app/views/search-sum/search-sum.component';
 import { SumHistoricalMatchComponent } from 'app/views/search-sum/sum-historical/match/historical-match.component';
+import { SumHistoricalComponent } from 'app/views/search-sum/sum-historical/sum-historical.component';
 
 export const LayoutRoutes: Routes = [
     { path: 'dashboard',      component: DashboardComponent },
@@ -21,5 +22,6 @@ export const LayoutRoutes: Routes = [
     { path: 'settings',  component: SettingsComponent },
     { path: 'statics/:option',  component: StaticsComponent },
     { path: 'search',  component: SearchSumComponent },
-    { path: 'sum-historical/:entrieId/:sumId',  component: SumHistoricalMatchComponent },
+    { path: 'sum-historical/:sumId',  component: SumHistoricalComponent },
+    { path: 'sum-historical-match/:entrieId/:sumId',  component: SumHistoricalMatchComponent },
 ];
