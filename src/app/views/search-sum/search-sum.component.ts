cee656ffe63fd1729f8c1c7ecdf9dfecbed9cfc2
Daniel Rodriguez <drg809@gmail.com>
2020-08-22 07:34:19 +0200

    search engine

--- src/app/views/search-sum/search-sum.component.ts
+++ src/app/views/search-sum/search-sum.component.ts
@@ -1,3 +1,4 @@
+import { SummonersStats } from './../../shared/models/summonersStats';
 import { Component, OnInit } from '@angular/core';
 import { SummonerService } from '../../shared/services/summoners.service';
 import { Router } from '@angular/router';
@@ -11,12 +12,35 @@ import { User } from 'app/shared/models/user';
 })
 export class SearchSumComponent implements OnInit {
   user: User;
+  sum: SummonersStats[];
+  searchQuery: string;
 
   constructor(private summonerService: SummonerService,
               private router: Router) { }
 
   ngOnInit() {
     this.user = JSON.parse(localStorage.getItem('currentUser') || '{}');
+  }
+
+  searchSum() {
+    this.summonerService.getStatsByName({name: this.searchQuery}).subscribe(x => {
+      this.sum = x;
+      console.log(this.sum);
+    });
+  }
+
+  showGraphics(name: string) {
+    this.summonerService.getStatsByName({name: name}).subscribe(x => {
+      this.sum = x;
+      console.log(this.sum);
+    });
+  }
+
+  compare(name: string) {
+    this.summonerService.getStatsByName({name: name}).subscribe(x => {
+      this.sum = x;
+      console.log(this.sum);
+    });
   }
 
   navigateToMatch(sumId: string) {
