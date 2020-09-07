46900d03d9f9228092f21b73484b11e7ae9b9ee5
Daniel Rodriguez <drg809@gmail.com>
2020-09-07 10:53:29 +0200

    rename file to use scss

--- src/app/components/account/login/login.component.ts
+++ src/app/components/account/login/login.component.ts
@@ -11,7 +11,7 @@ import { AuthGuard } from 'app/shared/helpers/auth.guard';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.scss'],
   animations: [
     trigger('formState', [
       transition('* => error', [
